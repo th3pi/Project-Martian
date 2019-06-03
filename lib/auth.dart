005a6bdd0ab680f5883005e60027592506105d58
th3pi <th3pizzaman@gmail.com>
2019-06-02 23:45:26 -0400

    Login page UI complete

--- lib/auth.dart
+++ lib/auth.dart
@@ -7,8 +7,15 @@ class Auth extends StatefulWidget {
   }
 }
 
+enum FormMode { LOGIN, SIGNUP }
+
 class _AuthState extends State<Auth> {
+  final _formKey = GlobalKey<FormState>();
+  String _errorMessage = '';
   String _email, _password;
+  FocusNode secondNode = FocusNode();
+  FormMode _formMode;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -25,18 +32,44 @@ class _AuthState extends State<Auth> {
               fontWeight: FontWeight.bold)),
         ),
       ),
-      body: _showLogo(),
+      body: _showBody(),
+    );
+  }
+
+  Widget _showBody() {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          shrinkWrap: false,
+          children: <Widget>[
+            _showLogo(),
+            _showEmailInput(),
+            _showPasswordInput(),
+            _showPrimaryButton(),
+            _showSecondaryButton(),
+            _showErrorMessage()
+          ],
+        ),
+      ),
     );
   }
 
   Widget _showLogo() {
     return Hero(
       tag: 'Hero',
-      child: Container(
-        padding: EdgeInsets.fromLTRB(0, 70, 0, 0),
-        child: Text(
-          'Martian',
-          style: TextStyle(color: Colors.white, fontFamily: 'SamsungOne'),
+      child: Center(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(0, 70, 0, 0),
+          child: Text(
+            'Martian',
+            style: TextStyle(
+                fontSize: 50,
+                color: Colors.white,
+                fontFamily: 'SamsungOne',
+                fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
@@ -44,15 +77,115 @@ class _AuthState extends State<Auth> {
 
   Widget _showEmailInput() {
     return Container(
-      padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [BoxShadow(color: Colors.white, offset: Offset(2, 0), blurRadius: 5)]),
+      margin: const EdgeInsets.fromLTRB(0, 65, 0, 0),
+      padding: const EdgeInsets.fromLTRB(10, 0, 10, 5),
       child: TextFormField(
         maxLines: 1,
         keyboardType: TextInputType.emailAddress,
         autofocus: false,
         decoration: InputDecoration(labelText: 'Email'),
-        validator: (String value) => value.isEmpty ? 'Email can\'t be empty' : null,
-        onSaved: (value) => _email = value,
+        validator: (String value) =>
+            value.isEmpty ? 'Email can\'t be empty' : null,
+        onSaved: (value) {
+          FocusScope.of(context).requestFocus(secondNode);
+          _email = value;
+        },
       ),
     );
+  }
+
+  Widget _showPasswordInput() {
+    bool _showPassword = false;
+    return Container(decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [BoxShadow(color: Colors.white, offset: Offset(2, 0), blurRadius: 5)]),
+      margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+      padding: const EdgeInsets.fromLTRB(10, 0, 10, 5),
+      child: TextFormField(
+        maxLines: 1,
+        obscureText: true,
+        focusNode: secondNode,
+        decoration: InputDecoration(
+          labelText: 'Password',
+        ),
+        validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+        onSaved: (value) => _password = value,
+      ),
+    );
+  }
+
+  Widget _showPrimaryButton() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+      child: RaisedButton(
+        elevation: 5,
+        color: Colors.white,
+        child: _formMode == FormMode.LOGIN
+            ? Text(
+                'Login',
+                style: TextStyle(fontSize: 20),
+              )
+            : Text(
+                'Create Account',
+                style: TextStyle(fontSize: 20),
+              ),
+        onPressed: () {
+//          _validateAndSubmit;
+        },
+      ),
+    );
+  }
+
+  Widget _showSecondaryButton() {
+    return FlatButton(
+      child: _formMode == FormMode.LOGIN
+          ? Text('Create an account')
+          : Text('Have an account? Sign in'),
+      onPressed: () {
+        if (_formMode == FormMode.LOGIN) {
+          _changeFormToSignUp();
+        } else {
+          _changeFormToLogin();
+        }
+      },
+    );
+  }
+
+  Widget _changeFormToSignUp() {
+    _formKey.currentState.reset();
+    _errorMessage = '';
+    setState(() {
+      _formMode = FormMode.SIGNUP;
+    });
+  }
+
+  Widget _changeFormToLogin() {
+    _formKey.currentState.reset();
+    _errorMessage = '';
+    setState(() {
+      _formMode = FormMode.LOGIN;
+    });
+  }
+
+  Widget _showErrorMessage() {
+    if (_errorMessage.length > 0 && _errorMessage != null) {
+      return AlertDialog(
+        title: Text('Oops'),
+        content: Text('Something went wrong'),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('OK'),
+          )
+        ],
+      );
+    } else {
+      return Container();
+    }
   }
 }
