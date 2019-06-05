4d45d1003e06db8960fda367b5a808772d93edf2
th3pi <th3pizzaman@gmail.com>
2019-06-04 22:55:17 -0400

    Added logged in homepage

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   String _email;
 
   HomePage(this._email);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Welcome $_email'),),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome $_email'),
+      ),
+    );
   }
 }
